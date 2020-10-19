3118a01c54c78dd982bfec9f1532b1a7160da904
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-19 16:51:24 +0300

    Configuration for select object added

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ import {Select} from "./select/select";
 import './select/styles.scss';
 
 const select = new Select('#select', {
-
+    placeholder: 'Please choose an element',
+    data: [
+        {id: '1', value: 'First'},
+        {id: '2', value: 'Second'},
+        {id: '3', value: 'Third'},
+        {id: '4', value: 'Fourth'},
+        {id: '5', value: 'Fifth'},
+        {id: '6', value: 'Sixth'},
+        {id: '7', value: 'Seventh'},
+    ],
 });
+
+window.s = select;
