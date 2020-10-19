7ab8bc569a8c177ec3b6568be70786627000f352
Roman Kashigin <r.kashigin@yandex.ru>
2020-10-19 16:25:06 +0300

    Click handler, toggle method, isOpen method and #setup method were added

--- select/select.js
+++ select/select.js
@@ -1,8 +1,8 @@
 const getTemplate = () => {
     return `
-        <div class="select__input">
+        <div class="select__input" data-type="input">
             <span>Text</span>
-            <i class="fa fa-chevron-down"></i>
+            <i class="fa fa-chevron-down" data-type="arrow"></i>
         </div>
         <div class="select__dropdown">
             <ul class="select__list">
@@ -23,17 +23,49 @@ export class Select {
     constructor(selector, options) {
         this.$el = document.querySelector(selector);
         this.#render();
+        this.#setup();
     }
 
     #render() {
+        this.$el.classList.add('select');
         this.$el.innerHTML = getTemplate();
+    }
+
+    #setup() {
+        this.clickHandler = this.clickHandler.bind(this);
+        this.$el.addEventListener('click', this.clickHandler);
+        this.$arrow = this.$el.querySelector('[data-type="arrow"]');
+    }
+
+    clickHandler(event) {
+        const {type} = event.target.dataset;
+
+        if (type === 'input') {
+            this.toggle();
+        }
+    }
+
+    get isOpen() {
+        return this.$el.classList.contains('open');
+    }
+
+    toggle() {
+        this.isOpen ? this.close() : this.open();
     }
 
     open() {
         this.$el.classList.add('open');
+        this.$arrow.classList.remove('fa-chevron-down');
+        this.$arrow.classList.add('fa-chevron-up');
     }
 
     close() {
         this.$el.classList.remove('open');
+        this.$arrow.classList.add('fa-chevron-down');
+        this.$arrow.classList.remove('fa-chevron-up');
+    }
+
+    destroy() {
+        this.$el.removeEventListener('click', this.clickHandler);
     }
 }
